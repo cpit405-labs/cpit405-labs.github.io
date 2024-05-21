0882a197536083250cb6428bbde255ac21de41de
abdulrahman abdullah alzahrani <aalialzahrani0014@stu.kau.edu.sa>
2024-05-21 22:17:42 +0300

    btn init

--- myWork/labs/lab5/script.js
+++ myWork/labs/lab5/script.js
@@ -1,8 +1,18 @@
 let main = document.getElementById('photos');
 
-
+// img creation
 let photo = document.createElement('img');
 photo.src = 'public/images/1.jpg';
 
-
+// attach img to main
 main.appendChild(photo);
+
+
+// btn creation
+let dbtn = document.createElement('button');
+dbtn.innerText = 'Delete';
+dbtn.onclick = function(){        
+    main.removeChild(photo);
+    main.removeChild(dbtn);
+}
+main.appendChild(dbtn);
