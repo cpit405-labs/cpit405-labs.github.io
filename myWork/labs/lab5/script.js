6ededdc9b47a6517b25a902f7843daeab0c18f78
abdulrahman abdullah alzahrani <aalialzahrani0014@stu.kau.edu.sa>
2024-05-21 22:45:18 +0300

    lab 5

--- myWork/labs/lab5/script.js
+++ myWork/labs/lab5/script.js
@@ -2,7 +2,9 @@ let main = document.getElementById('photos');
 
 // img creation
 let photo = document.createElement('img');
-photo.src = 'public/images/1.jpg';
+photo.src = 'public/imgs/1.jpg';
+photo.width = 70;
+photo.height = 120;
 
 // attach img to main
 main.appendChild(photo);
@@ -13,6 +15,40 @@ let dbtn = document.createElement('button');
 dbtn.innerText = 'Delete';
 dbtn.addEventListener("click", function(){        
     main.removeChild(photo);
-    this.removeChild(dbtn); // referint to the button(dbtn)
+    main.removeChild(this); // referint to the button(dbtn)
+    main.removeChild(zoomin);
+    main.removeChild(zoomout);
 });
 main.appendChild(dbtn);
+
+
+// zooms btn creation
+let zoomin = document.createElement('button');
+zoomin.innerText = '+';
+let zoomout = document.createElement('button');
+zoomout.innerText = '-';
+main.appendChild(zoomin);
+main.appendChild(zoomout);
+
+// zoom in
+zoomInScale = 1.5;
+zoomin.addEventListener("click", function(){
+    photo.width *= zoomInScale;
+    photo.height *= zoomInScale;
+});
+
+// zoom out 
+zoomoutScale = 0.5;
+zoomout.addEventListener("click", function(){
+    photo.width *= zoomoutScale;
+    photo.height *= zoomoutScale;
+});
+
+// create a new img
+let userPhoto = document.createElement('input');
+userPhoto.setAttribute('type', 'file' );
+userPhoto.setAttribute('accept', 'image/*');
+userPhoto.src = userPhoto.value;
+userPhoto.width = 70;
+userPhoto.height = 120;
+main.appendChild(userPhoto);
